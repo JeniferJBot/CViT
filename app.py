70bf87aa139d68f0fcf0d8b0e3ffdbe586bc174b
JeniferJBot <jeni.priya.devan@gmail.com>
2026-03-03 12:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-import streamlit as st
-from cvit_prediction import predict_video   # adjust if function name differs
 import os
 import urllib.request
+import streamlit as st
 
+# ---------------- DOWNLOAD MODEL FIRST ----------------
 MODEL_URL = "https://huggingface.co/mhamza-007/cvit_deepfake_detection/resolve/main/cvit2_deepfake_detection_ep_50.pth"
 MODEL_PATH = "weight/cvit2_deepfake_detection_ep_50.pth"
 
-# download model automatically
 if not os.path.exists(MODEL_PATH):
     os.makedirs("weight", exist_ok=True)
-    print("Downloading model... (first run only)")
+    st.write("Downloading model... (first run only, please wait ⏳)")
     urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
 
+# ---------------- IMPORT AFTER DOWNLOAD ----------------
+from cvit_prediction import predict_video
+
+# ---------------- STREAMLIT UI ----------------
 st.title("Deepfake Video Detection System")
 
 uploaded_file = st.file_uploader(
